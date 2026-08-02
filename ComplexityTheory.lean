49ce23205129baf492b0602098171c887ebfb0ec
windsornguyen <win@dedaluslabs.ai>
2026-08-02 11:37:32 -0700

    feat(formulas): restrict variable prefixes

--- ComplexityTheory.lean
+++ ComplexityTheory.lean
@@ -2,6 +2,7 @@ import ComplexityTheory.Foundations.BinaryString
 import ComplexityTheory.Foundations.NatEncoding
 import ComplexityTheory.Foundations.BooleanFormula
 import ComplexityTheory.Foundations.FormulaEncoding
+import ComplexityTheory.Foundations.FormulaRestriction
 
 /-!
 # ComplexityTheory
