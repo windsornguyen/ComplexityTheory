8e15f408dbd4f60307b6487e4f66b888d5f4b529
windsornguyen <win@dedaluslabs.ai>
2026-08-02 11:27:22 -0700

    feat(encodings): compile formula postfix streams

--- ComplexityTheory.lean
+++ ComplexityTheory.lean
@@ -1,7 +1,7 @@
 import ComplexityTheory.Foundations.BinaryString
 import ComplexityTheory.Foundations.NatEncoding
 import ComplexityTheory.Foundations.BooleanFormula
-import ComplexityTheory.Foundations.FormulaEncoding.Token
+import ComplexityTheory.Foundations.FormulaEncoding.Stream
 
 /-!
 # ComplexityTheory
